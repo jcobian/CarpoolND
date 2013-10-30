a46ceab63f48e6938bbb9d3ea6205a8f5430a181
jcobian <jcobian@nd.edu>
2013-10-30 01:54:17 -0400

    deleted cars and other stuff

--- showDeletedCar.php
+++ showDeletedCar.php
@@ -9,23 +9,20 @@ p.Helv{font-family:Helvetica,serif}
 $Make = $_GET['Make'];
 $Model = $_GET['Model'];
 $Year = $_GET['Year'];
-$Owner = $_GET['owner'];
-$Numberofseats = $_GET['Numberofseats'];
 $c = oci_connect('jwassel', 'jasonwassel', '//localhost/curt');
 if (!$c) {
     $e = oci_error();   // For oci_connect errors do not pass a handle
     trigger_error(htmlentities($e['message']), E_USER_ERROR);
 }
-$q = 'insert into car (car_id, make, model, year, owner, numberofseats) values (seq_car_id.nextval, :v, :w, :x, :y, :z)';
+#$q = "delete from car where make='".$Make."' AND model='".$Model."'AND year='".$YEAR."'";
+$q = "delete from car where make= :v and model= :t";
 //Parse that SQL query into a statement
 $s = oci_parse($c, $q);
 
 //Next we bind the variables to the placeholders.
 oci_bind_by_name($s, ":v", $Make);
-oci_bind_by_name($s, ":w", $Model);
-oci_bind_by_name($s, ":x", $Year);
-oci_bind_by_name($s, ":y", $Owner);
-oci_bind_by_name($s, ":z", $Numberofseats);
+oci_bind_by_name($s, ":t", $Model);
+
 //Execute the SQL statement
 if(!oci_execute($s)){
 $e = oci_error($s);
