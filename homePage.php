a0e06b023f5dd1d609297b3cb1c431319d50a0ab
jcobian <jcobian@nd.edu>
2013-10-30 20:59:33 -0400

    restructured how the forms are written aka added labels, also changed so pages are on jcobian

--- homePage.php
+++ homePage.php
@@ -16,18 +16,18 @@ $username = $_POST['username'];
 $password = $_POST['password'];
 ?>
 <center>
-<div style="background-color:#ffcc00">
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/insertCar.html">Add a Car</a>
+<div id="homePageLinksDiv" style="background-color:#ffcc00">
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/insertCar.html">Add a Car</a>
 <br></br>
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/insertCarpool.html">Create a Carpool</a>
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/insertCarpool.html">Create a Carpool</a>
 <br></br>
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/insertSearcher.html">Request a ride</a>
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/insertSearcher.html">Request a ride</a>
 <br></br>
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/deleteCar.php">Remove one of my cars</a>
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/deleteCar.php">Remove one of my cars</a>
 <br></br>
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/updateTime.php">Change Time of Carpool</a>
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/updateTime.php">Change Time of Carpool</a>
 <br></br>
-<a href="http://orchestra.cselab.nd.edu/~jwassel/CarpoolND/searchCarpool.php">Show All Carpools</a>
+<a href="http://orchestra.cselab.nd.edu/~jcobian/CarpoolND/searchCarpool.php">Show All Carpools</a>
 <br></br>
 </div>
 </center>
