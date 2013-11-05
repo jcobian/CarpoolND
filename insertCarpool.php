87119a98b3d7a74be08360ced177b6e4288cae24
Jason Martin Wassel <jwassel@nd.edu>
2013-11-04 23:37:03 -0500

    format dates

--- insertCarpool.php
+++ insertCarpool.php
@@ -10,9 +10,12 @@ p.Helv{font-family:Helvetica,serif}
 
 $startingPoint = $_GET['startingPoint'];
 $destination = $_GET['destination'];
-$month= $_GET['month'];
-$day= $_GET['day'];
-$year= $_GET['year'];
+$startMonth= $_GET['startMonth'];
+$startDay= $_GET['startDay'];
+$startYear= $_GET['startYear'];
+$endNonth= $_GET['endMonth'];
+$endDay= $_GET['endDay'];
+$endYear= $_GET['endYear'];
 $startHour = $_GET['startHour'];
 $startMinute= $_GET['startMinute'];
 $startAmPm = $_GET[startAmPm'];
@@ -21,6 +24,14 @@ $endMinute= $_GET['endMinute'];
 $endAmPm = $_GET[endAmPm'];
 $description = $_GET['description'];
 $openSeats = $_GET['openSeats'];
+$passenger1 = $_GET['passenger1'];
+$passenger2 = $_GET['passenger2'];
+$passenger3 = $_GET['passenger3'];
+$passenger4 = $_GET['passenger4'];
+$passenger5 = $_GET['passenger5'];
+$passenger6 = $_GET['passenger6'];
+$passenger7 = $_GET['passenger7'];
+$passenger8 = $_GET['passenger8'];
 $carId = $_GET['carId'];
 
 $c = oci_connect('jwassel', 'jasonwassel', '//localhost/curt');
@@ -29,7 +40,7 @@ if (!$c) {
     trigger_error(htmlentities($e['message']), E_USER_ERROR);
 }
 
-$q = 'insert into carpool (carpool_id,car_id, startingpoint, destination, startdate, starttime, eta, description, openseats, driver, user1, user2, user3, user4, user5, user6, user7, user8) values (seq_carpool_id.nextval, :f, :g, :h, :j, :k, :l, :m, :n, :p, :q, :r, :t, :v, :w, :x, :y, :z)';
+$q = 'insert into carpool (carpool_id,car_id, startingpoint, destination, startDate, endDate, description, openseats) values (seq_carpool_id.nextval, :f, :g, :h, :j, :k, :l, :m, :n, :p)';
 //Parse that SQL query into a statement
 $s = oci_parse($c, $q);
 
@@ -39,19 +50,10 @@ oci_bind_by_name($s, ":f", $carId);
 oci_bind_by_name($s, ":g", $startingPoint);
 oci_bind_by_name($s, ":h", $destination);
 oci_bind_by_name($s, ":j", $startDate);
-oci_bind_by_name($s, ":k", $startTime);
-oci_bind_by_name($s, ":l", $ETA);
+oci_bind_by_name($s, ":l", $endDate);
 oci_bind_by_name($s, ":m", $description);
 oci_bind_by_name($s, ":n", $openSeats);
 oci_bind_by_name($s, ":p", $_SESSION['username']);
-oci_bind_by_name($s, ":q", $passenger1);
-oci_bind_by_name($s, ":r", $passenger2);
-oci_bind_by_name($s, ":t", $passenger3);
-oci_bind_by_name($s, ":v", $passenger4);
-oci_bind_by_name($s, ":w", $passenger5);
-oci_bind_by_name($s, ":x", $passenger6);
-oci_bind_by_name($s, ":y", $passenger7);
-oci_bind_by_name($s, ":z", $passenger8);
 //Execute the SQL statement
 if(!oci_execute($s))
 {
