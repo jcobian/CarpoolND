b8ee15021d469eda215167bba1b33774c76d9814
jcobian <jcobian@nd.edu>
2013-11-18 02:53:02 -0500

    added user settings page, split up where some of the buttons go

--- homePage.php
+++ homePage.php
@@ -8,23 +8,18 @@ $username = $_SESSION['username'];
 ?>
 <script type="text/javascript">
 function clickedButton(sel) {
-	if(sel.id=="addCar") {
-		window.location.href="insertCar.html";
-	}
 	if(sel.id=="createCarpool") {
 		window.location.href="createCarpool.php";
 	}
 	if(sel.id=="insertSearcher") {
 		window.location.href="insertSearcher.html";
 	}
-	if(sel.id=="deleteCar") {
-		window.location.href="deleteCar.php";
-	}
-	if(sel.id=="updateTime") {
-		window.location.href="updateTime.php";
-	}
 	if(sel.id=="searchCarpools") {
 		window.location.href="searchCarpool.php";
+	}
+	if(sel.id=="userSettings") {
+		window.location.href="userSettings.php";
+
 	}	
 
 
@@ -33,16 +28,12 @@ function clickedButton(sel) {
 </head>
 <body>
 <table width="100%" >
-<tr><td align="center"><h2>CarpoolND</h2> </td><td> <h4>User: <?php print $username ?></h4> </td>  </tr>
+<tr><td align="center"><h2>CarpoolND</h2> </td><td> <h4>User: <?php print $username ?></h4> <button id="userSettings"type="button" onclick="clickedButton(this)"> Settings</button> </td>  </tr>
 </table>
 <table>
-<tr><td> <button id="addCar" type="button" onclick="clickedButton(this)"> Add a Car</button></td></tr>
-<tr><td> <button id="createCarpool" type="button" onclick="clickedButton(this)"> Create Carpool</button></td></tr>
-<tr><td> <button id="insertSearcher" type="button" onclick="clickedButton(this)"> Request a ride</button></td></tr>
-<tr><td> <button id="deleteCar" type="button" onclick="clickedButton(this)"> Remove one of my cars</button></td></tr>
-<tr><td> <button id="updateTime" type="button" onclick="clickedButton(this)"> Change Carpool Time</button></td></tr>
-<tr><td> <button id="searchCarpools" type="button" onclick="clickedButton(this)"> Search For Carpools</button></td></tr>
-
+<tr><td> <button id="createCarpool" type="button" onclick="clickedButton(this)"> I Am a Driver</button></td></tr>
+<tr><td> <button id="insertSearcher" type="button" onclick="clickedButton(this)">  I am a Passenger</button></td></tr>
+<tr><td> <button id="searchCarpools" type="button" onclick="clickedButton(this)"> Browse All Carpools</button></td></tr>
 </table>
 </body>
 </html>
---
+++ userSettings.php
@@ -0,0 +1,35 @@
+<?php session_start();?>
+<html>
+<head>
+<title> CarpoolND: User Settings </title>
+</head>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<?php
+$username = $_SESSION['username'];
+?>
+<script type="text/javascript">
+function clickedButton(sel) {
+	if(sel.id=="addCar") {
+		window.location.href="insertCar.html";
+	}
+	if(sel.id=="deleteCar") {
+		window.location.href="deleteCar.php";
+	}
+	if(sel.id=="updateTime") {
+		window.location.href="updateTime.php";
+	}
+
+}
+</script>
+
+<body>
+<h2 align="center"> <?php print $username ?> </h2>
+
+<table>
+<tr><td> <button id="addCar" type="button" onclick="clickedButton(this)"> Add a Car</button></td></tr>
+<tr><td> <button id="deleteCar" type="button" onclick="clickedButton(this)"> Remove one of my cars</button></td></tr>
+<tr><td> <button id="updateTime" type="button" onclick="clickedButton(this)"> Change one of my carpool times</button></td></tr>
+</table>
+
+</body>
+</html>
