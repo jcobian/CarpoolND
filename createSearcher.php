7e194e93e16f7aa846e22a444c1b2968ce4de166
jcobian <jcobian@nd.edu>
2013-11-20 01:07:05 -0500

    added so that can insert into the searchers table

--- createSearcher.php
+++ createSearcher.php
@@ -11,15 +11,289 @@ exit;
 <head>
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
+ <style>
+      #map-canvas {
+        height: 90%;
+        margin-left: 0px;
+        padding: 10px;
+	width: 55%;
+	float:right
+      }
+    </style>
+    <script src="https://maps.googleapis.com/maps/api/js?v=3key={AIzaSyA5NX_2Kc2TG3t9vePL-3gz2W6YAhoB1tA}&sensor=false">></script>
+    <script>
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
+var map;
+var geocoder;
+window.onload = function() {
+
+ updateTime();
+
+}
+function updateTime() {
+var today = new Date();
+  var dd = today.getDate(); 
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
+  document.getElementById("startMonth").value = mm;
+  document.getElementById("startDay").value = dd;
+  document.getElementById("startYear").value = yyyy;
+
+  document.getElementById("endMonth").value = mm;
+  document.getElementById("endDay").value = dd;
+  document.getElementById("endYear").value = yyyy;
+
+}
+function initialize() {
+  
+  geocoder = new google.maps.Geocoder();
+
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var chicago = new google.maps.LatLng(41.850033, -87.6500523);
+  var mapOptions = {
+    zoom:7,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    center: chicago
+  }
+  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  directionsDisplay.setMap(map);
+}
+
+function calcRoute() {
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  var request = {
+      origin:start,
+      destination:end,
+      travelMode: google.maps.DirectionsTravelMode.DRIVING
+  };
+  directionsService.route(request, function(response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+    }
+  });
+}
+
+  function codeAddress(sel) {
+    var isEnd;
+    var address;
+    if(sel.id == 'start') {
+	isEnd = 0;
+        address = document.getElementById('start').value;}
+    else { 
+        isEnd = 1;
+        address = document.getElementById('end').value;
+    }
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+	if(isEnd==0) {
+		document.getElementById("startLat").value = results[0].geometry.location.lat()
+		document.getElementById("startLong").value = results[0].geometry.location.lng()
+	} else {
+		document.getElementById("endLat").value = results[0].geometry.location.lat()
+		document.getElementById("endLong").value = results[0].geometry.location.lng()
+	}
+      	//alert("Position is: " + results[0].geometry.location.lat() + "," + results[0].geometry.location.lng());
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+  }
+
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
+    </script>
 </head>
 <body>
 <center><h2> Request a Ride</h2></center>
+    <div id="map-canvas"></div>
 <form action="insertSearcher.php" method="get">
-<div class="insertForm">
-<div class="form-group"><label for="startDate">Start Date: <input name="startDate"/></div>
-<div class="form-group"><label for="startingPoint">Starting Point: <input name="startingPoint"/></div>
-<div class="form-group"><label for="destination">Destination: <input name="destination"/></div>
-<div class="form-group"><label for="description">Description: <input name="description"/></div>
+<div class="insertForm" style="float:left">
+<div class="form-group"><label for="start">Starting Point: <input name="start" id="start" type="textbox" onchange="calcRoute(); codeAddress(this);"/></div>
+<div class ="form-group"><label for="end">End Point: <input name="end" id="end" onchange="calcRoute(); codeAddress(this);"/></div>
+<input name="startLat" id="startLat" type="hidden"/>
+<input name="startLong" id="startLong" type="hidden"/>
+<input name="endLat" id="endLat" type="hidden"/>
+<input name="endLong" id="endLong" type="hidden"/>
+<div class = "form-control">
+<span>Start Date: </span>
+<select name="startMonth" id = "startMonth">
+<option value=01> January </option>
+<option value=02> February </option>
+<option value=03> March </option>
+<option value=04> April </option>
+<option value=05> May </option>
+<option value=06> June </option>
+<option value=07> July </option>
+<option value=08> August </option>
+<option value=09> September </option>
+<option value=10> October </option>
+<option value=11> November </option>
+<option value=12> December </option>
+</select>
+<select name="startDay" id = "startDay">
+<option value=01> 1 </option>
+<option value=02> 2 </option>
+<option value=03> 3 </option>
+<option value=04> 4 </option>
+<option value=05> 5 </option>
+<option value=06> 6 </option>
+<option value=07> 7 </option>
+<option value=08> 8 </option>
+<option value=09> 9 </option>
+<option value=10> 10 </option>
+<option value=11> 11 </option>
+<option value=12> 12 </option>
+<option value=13> 13 </option>
+<option value=14> 14 </option>
+<option value=15> 15 </option>
+<option value=16> 16 </option>
+<option value=17> 17 </option>
+<option value=18> 18 </option>
+<option value=19> 19 </option>
+<option value=20> 20 </option>
+<option value=21> 21 </option>
+<option value=22> 22 </option>
+<option value=23> 23 </option>
+<option value=24> 24 </option>
+<option value=25> 25 </option>
+<option value=26> 26 </option>
+<option value=27> 27 </option>
+<option value=28> 28 </option>
+<option value=29> 29 </option>
+<option value=30> 30 </option>
+<option value=31> 31 </option>
+</select>
+<select name="startYear" id = "startYear">
+<option value=2013>2013</option>
+<option value=2014>2014</option>
+<option value=2015>2015</option>
+<option value=2016>2016</option>
+</select>
+</div>
+<div class = "form-control">
+<span>Start Time: </span>
+<select name="startHour" id = "startHour">
+<option value=1> 1 </option>
+<option value=2> 2 </option>
+<option value=3> 3 </option>
+<option value=4> 4 </option>
+<option value=5> 5 </option>
+<option value=6> 6 </option>
+<option value=7> 7 </option>
+<option value=8> 8 </option>
+<option value=9> 9 </option>
+<option value=10> 10 </option>
+<option value=11> 11 </option>
+<option value=12 selected> 12 </option>
+</select>
+<span>:</span>
+<select name="startMinute" id = "startMinute">
+<option value=00 selected> 00 </option>
+<option value=15> 15 </option>
+<option value=30> 30 </option>
+<option value=45> 45 </option>
+</select>
+<select name="startAmPm" id = "startAmPm">
+<option value="am"> AM </option>
+<option value="pm" selected> PM </option>
+</select>
+</div>
+<div class = "form-control">
+<span>ETA Date: </span>
+<select name="endMonth" id = "endMonth">
+<option value=01> January </option>
+<option value=02> February </option>
+<option value=03> March </option>
+<option value=04> April </option>
+<option value=05> May </option>
+<option value=06> June </option>
+<option value=07> July </option>
+<option value=08> August </option>
+<option value=09> September </option>
+<option value=10> October </option>
+<option value=11> November </option>
+<option value=12> December </option>
+</select>
+<select name="endDay" id = "endDay">
+<option value=01> 1 </option>
+<option value=02> 2 </option>
+<option value=03> 3 </option>
+<option value=04> 4 </option>
+<option value=05> 5 </option>
+<option value=06> 6 </option>
+<option value=07> 7 </option>
+<option value=08> 8 </option>
+<option value=09> 9 </option>
+<option value=10> 10 </option>
+<option value=11> 11 </option>
+<option value=12> 12 </option>
+<option value=13> 13 </option>
+<option value=14> 14 </option>
+<option value=15> 15 </option>
+<option value=16> 16 </option>
+<option value=17> 17 </option>
+<option value=18> 18 </option>
+<option value=19> 19 </option>
+<option value=20> 20 </option>
+<option value=21> 21 </option>
+<option value=22> 22 </option>
+<option value=23> 23 </option>
+<option value=24> 24 </option>
+<option value=25> 25 </option>
+<option value=26> 26 </option>
+<option value=27> 27 </option>
+<option value=28> 28 </option>
+<option value=29> 29 </option>
+<option value=30> 30 </option>
+<option value=31> 31 </option>
+</select>
+<select name="endYear" id = "endYear">
+<option value=2013>2013</option>
+<option value=2014>2014</option>
+<option value=2015>2015</option>
+<option value=2016>2016</option>
+</select>
+</div>
+<div class = "form-control">
+<span>ETA: </span>
+<select name="endHour" id = "endHour">
+<option value=1> 1 </option>
+<option value=2> 2 </option>
+<option value=3> 3 </option>
+<option value=4> 4 </option>
+<option value=5> 5 </option>
+<option value=6> 6 </option>
+<option value=7> 7 </option>
+<option value=8> 8 </option>
+<option value=9> 9 </option>
+<option value=10> 10 </option>
+<option value=11> 11 </option>
+<option value=12 selected> 12 </option>
+</select>
+<span>:</span>
+<select name="endMinute" id = "endMinute">
+<option value=00 selected> 00 </option>
+<option value=15> 15 </option>
+<option value=30> 30 </option>
+<option value=45> 45 </option>
+</select>
+<select name="endAmPm" id = "endAmPm">
+<option value="am"> AM </option>
+<option value="pm" selected> PM </option>
+</select>
+</div>
+
+
+
+
 <input type="submit"/>
 </div>
 </form>
--- insertSearcher.php
+++ insertSearcher.php
@@ -9,16 +9,30 @@ exit;
 ?>
 <html>
 <head>
-<style>
-body {background-color:#041c42;}
-p {font-size:14px}
-p.Helv{font-family:Helvetica,serif}
-</style>
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
 <?php
-$startDate = $_GET['startDate'];
-$startingPoint = $_GET['startingPoint'];
-$destination = $_GET['destination'];
-$description = $_GET['description'];
+$startName = $_GET['start'];
+$endName = $_GET['end'];
+$startLat = $_GET['startLat'];
+$startLong = $_GET['startLong'];
+$endLat = $_GET['endLat'];
+$endLong = $_GET['endLong'];
+
+$startMonth= $_GET['startMonth'];
+$startDay= $_GET['startDay'];
+$startYear= $_GET['startYear'];
+$endMonth= $_GET['endMonth'];
+$endDay= $_GET['endDay'];
+$endYear= $_GET['endYear'];
+$startHour = $_GET['startHour'];
+$startMinute= $_GET['startMinute'];
+$startAmPm = $_GET['startAmPm'];
+$endHour = $_GET['endHour'];
+$endMinute= $_GET['endMinute'];
+$endAmPm = $_GET['endAmPm'];
+$startDateString = $startYear.'/'.$startMonth.'/'.$startDay.':'.$startHour.':'.$startMinute.':00'.$startAmPm;
+$endDateString = $endYear.'/'.$endMonth.'/'.$endDay.':'.$endHour.':'.$endMinute.':00'.$endAmPm;
 
 $c = oci_connect('jwassel', 'jasonwassel', '//localhost/curt');
 if (!$c) {
@@ -27,16 +41,21 @@ if (!$c) {
 }
 
 
-$q = 'insert into searchers (search_id, username, startdate, startingpoint, destination, description) values (seq_search_id.nextval, :f, :g, :h, :j, :k)';
+$q = "insert into searchers (search_id, username, startlat, startlng,endlat,endlng,startdate,enddate,startname,endname) values (seq_search_id.nextval, :f, :g, :h, :j, :k,to_date(:l,'yyyy/mm/dd:hh:mi:ssam'), to_date(:m,'yyyy/mm/dd:hh:mi:ssam'),:n,:o)";
 //Parse that SQL query into a statement
 $s = oci_parse($c, $q);
 
 //Next we bind the variables to the placeholders.
 oci_bind_by_name($s, ":f", $_SESSION['username']);
-oci_bind_by_name($s, ":g", $startDate);
-oci_bind_by_name($s, ":h", $startingPoint);
-oci_bind_by_name($s, ":j", $destination);
-oci_bind_by_name($s, ":k", $description);
+oci_bind_by_name($s, ":g", $startLat);
+oci_bind_by_name($s, ":h", $startLong);
+oci_bind_by_name($s, ":j", $endLat);
+oci_bind_by_name($s, ":k", $endLong);
+oci_bind_by_name($s, ":l", $startDateString);
+oci_bind_by_name($s, ":m", $endDateString);
+oci_bind_by_name($s, ":n", $startName);
+oci_bind_by_name($s, ":o", $endName);
+
 
 //Execute the SQL statement
 if(!oci_execute($s))
@@ -45,9 +64,10 @@ if(!oci_execute($s))
 	print $e['message'];
 }
 oci_close($c);
-header("Location:homePage.php");
+//header("Location:homePage.php");
 ?>
 </head>
 <body>
+<p> add a table here that displays results. maybe don't do oci_close and then right here you set up the table then do the search and add it to the table </p>
 </body>
 </html>
