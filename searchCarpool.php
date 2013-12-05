12192c4e35fe38932b7865ca70582abd4da89c0e
oliverlamb <olamb@nd.edu>
2013-12-05 01:55:06 -0500

    ability to request to join a carpool

--- searchCarpool.php
+++ searchCarpool.php
@@ -7,13 +7,13 @@ header("Location:index.html");
 exit;
 }
 if(!empty($_GET['startLocation']))
-	$startLocation = $_GET['startLocation'];
+	$startLocation = strtoupper($_GET['startLocation']);
 if(!empty($_GET['endLocation']))
-	$endLocation = $_GET['endLocation'];
+	$endLocation = strtoupper($_GET['endLocation']);
 if(!empty($_GET['Make']))
-	$Make = $_GET['Make'];
+	$Make = strtoupper($_GET['Make']);
 if(!empty($_GET['Model']))
-	$Model = $_GET['Model'];
+	$Model = strtoupper($_GET['Model']);
 if(!empty($_GET['Year']))
 	$Year = $_GET['Year'];
 if(!empty($_GET['openSeats']))
@@ -26,23 +26,33 @@ if(!empty($_GET['openSeats']))
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <script type="text/javascript">
+window.onload = function() {
+  document.getElementById("Year").selectedIndex = -1;
+  document.getElementById("openSeats").selectedIndex = -1;
+}
 function clickedButton(sel) {
 	if(sel.id=="goBack") {
 		window.location.href="homePage.php";
 	}
 
 }
+function joinClicked(sel, email) {
+	var getValue = sel.id;
+	alert("A notification has been sent to " + email + "!");
+	window.location = "?getValue=" + getValue;
+
+}
 </script>
 </head>
 <body>
 <div style="background-color:#FFFFFF">
-	<form action="searchCarpool.php" method="get" role="form">
-	<div><label for="startLocation">Starting Location: </label><input type="text" name="startLocation"/>
+	<form action="searchCarpool.php" method="get" role="form" id="queryForm">
+	<div><label for="startLocation">Starting Location: </label><input type="text" name="startLocation" id="startLocation"/>
 	<label for="endLocation">Destination: </label><input type="text" name="endLocation"/></div>
 	<div><label for="Make">Make: </label><input name="Make"/>
 	<label for="Model">Model: </label><input name="Model"/>
 	<span>Min. Car Year: </span>
-	<select name="Year">
+	<select name="Year" id="Year">
 	<option value=2014>2014</option>
 	<option value=2013>2013</option>
 	<option value=2012>2012</option>
@@ -82,7 +92,7 @@ function clickedButton(sel) {
 	</select>
 	
 	<span>Min. Open Seats: </span>
-	<select name="openSeats">
+	<select name="openSeats" id="openSeats">
 	<option value=1> 1 </option>
 	<option value=2> 2 </option>
 	<option value=3> 3 </option>
@@ -96,14 +106,15 @@ function clickedButton(sel) {
 
 </div>
 	<input type="submit" />
-	</div>
-
+	<form action="searchCarpool.php" method="get" role="form"><input type="submit" value = "Clear"/></form>
 	</form>
+	
+	</div>
 </div>
 <div>
 <?php
 $c = oci_connect('jwassel', 'jasonwassel', '//localhost/curt');
-$q = 'select c.startname, c.endname, c.startdate, c.enddate, c.description, c.openseats, c.driver, c2.year, c2.make, c2.model from carpool c, car c2 where c.driver = c2.owner';
+$q = 'select c.carpool_id, c.driver, c.startname, c.endname, c.startdate, c.enddate, c.description, c.openseats, c2.year, c2.make, c2.model, u.firstname, u.lastname from carpool c, car c2, users u where c.driver = c2.owner AND c2.owner = u.username';
 if(!empty($_GET['startLocation']))
 	$q = $q.' AND c.startname = :t ';
 if(!empty($_GET['endLocation']))
@@ -126,31 +137,49 @@ oci_bind_by_name($s, ":x", $Model);
 oci_bind_by_name($s, ":y", $Year);
 oci_bind_by_name($s, ":z", $openSeats);
 //Execute the SQL statement/query
-print $q;
 oci_execute($s);
 
 //We want to print out the results of the query into an HTML table; so we'll need to write an HTML table. This can be done by closing the PHP and writing straight HTML code, or you can have PHP print the HTML code. Let's have PHP print the HTML for now.
 print '<table border = "1" style="background-color:#FFFFFF; overflow-y:scroll; height:auto; display:block;">';
 //We want to loop through all of the rows from the result of the query
           print '<tr>';
-  print '<td><b>Starting Point </b></td> <td><b>Destination</b></td> <td><b>Start Date</b></td> <td><b>End Date</b></td> <td><b>Description</b></td> <td><b>Open Seats</b></td> <td><b>Driver</b></td> <td><b>Car</b></td>';
+  print '<td><b>Starting Point </b></td> <td><b>Destination</b></td> <td><b>Start Date</b></td> <td><b>End Date</b></td> <td><b>Description</b></td> <td><b>Open Seats</b></td> <td><b>Car</b></td> <td><b>Driver</b></td> <td><b>Request to Join</b></td>';
 print '<tr>';
 while ($row = oci_fetch_array($s,OCI_ASSOC)) {
     //Inside the loop we have get one $row at a time, so we need to handle this with HTML:
 $len = count($row);
     print '<tr>';
     $i = 0;
+    $carpoolid = 0;
+    $email = "";
     //Inside of each $row we have several attributes/columns that we need to iterate through.
     foreach($row as $column) {
-    	if($i == $len-3)
+    	if($i == 0 || $i == 1)
+    	{
+    		if($i==0)
+    			$carpoolid = $column;
+    		if($i==1)
+    			$email = $column;
+    		$i = $i + 1;
+    		continue;
+    	}	
+    	if($i == $len-5)
     	{
     		print '<td>'.$column.' ';
     	}
-    	else if($i == $len-2)
+    	else if($i == $len-4)
     	{
     		print $column.' ';
     	}
-       	else if($i == $len-1)
+       	else if($i == $len-3)
+    	{
+    		print $column.'</td>';
+    	}
+    	else if($i == $len-2)
+    	{
+    		print '<td>'.$column.' ';
+    	}
+    	else if($i == $len-1)
     	{
     		print $column.'</td>';
     	}
@@ -161,6 +190,7 @@ $len = count($row);
         }
         $i = $i + 1;
     }
+    print '<td><button type ="button" onclick="joinClicked(this,\''.$email.'\')" id = "'.$carpoolid.'">Join</button></td>';
     //Thus ends the $row...
     print '<tr>';
 }
@@ -168,6 +198,17 @@ $len = count($row);
 print '</table>';
 
 //Now let's close the PHP code and end the HTML.
+?>
+<?php
+if(isset($_GET['getValue']))
+{
+	$carpid = $_GET['getValue'];
+	$q = 'insert into request(username, carpool_id) values(:t, :u)';
+	$s = oci_parse($c, $q);
+	oci_bind_by_name($s, ":t", $_SESSION['username']);
+	oci_bind_by_name($s, ":u", $carpid);
+	oci_execute($s);
+}
 ?>
 </div>
 <button id="goBack" type="button" onclick="clickedButton(this)"> Go Back</button>
